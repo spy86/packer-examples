d0d8effa4492475ba26ef83f56fb70d433991869
spy86 <test@localhost>
2021-04-18 14:00:38 +0200

    update configuration

--- virtualbox/centos8/scripts/ansible.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eux
-
-# Install Python.
-yum -y install python3 python3-pip python3-setuptools
-alternatives --set python /usr/bin/python3
-pip3 install --upgrade setuptools
-
-# Install Ansible.
-pip3 install ansible
